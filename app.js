8b680a6d055ef0ab8cea25196765b8376af8a976
Marius Banea <marius@w-d-s.co.uk>
2017-12-13 17:43:23 +0000

    updating the app.js

---
+++ app.js
@@ -0,0 +1,99 @@
+/*
+This app doesn't follow a11y best practices, and the JS file is incomplete.
+
+Complete the getDataFromApi, displaySearchData, and watchSubmit functions.
+
+When you're done, this app should allow a user to search for an artist and song title (both should be required), and display the song lyrics.
+
+You should make requests to this API: https://lyricsovh.docs.apiary.io/# .
+
+Also make any necessary adjustments to make this app accessible. */
+
+
+
+//Step 2 - define the function to make the api call; shopkeeper goes to warehouse to get shoe
+function getDataFromApi(queryTarget) {
+
+    //full AJAX intro https://www.w3schools.com/xml/ajax_intro.asp
+
+    // Step 2a - make the api call using the URL, dataType (JSON or JSONP), type (GET or POST)
+    $.ajax({
+            type: "GET",
+            url: 'https://dog.ceo/api/breed/' + queryTarget + '/images',
+            dataType: 'json',
+        })
+
+        //Step 2b - success scenario (call the function to display the results)
+        .done(function (dataOutput) {
+
+            //displays the external api json object in the console
+            displaySearchData(dataOutput);
+        })
+
+        // Step 2c - failure scenario (display errors)
+        .fail(function (jqXHR, error, errorThrown) {
+
+            //display errors
+            console.log(jqXHR);
+            console.log(error);
+            console.log(errorThrown);
+        });
+};
+
+
+//Step 3 - display the results; sales process
+function displaySearchData(data) {
+
+    //Step 3a - console.log the results
+    console.log(data);
+
+    //Step 3b - if there are no results show errors
+    if (data.message == "") {
+
+        //show and alert
+        alert("No results");
+    }
+
+    //
+    else {
+
+        //Step 3c - if there are results, create an HTML results variable
+        let htmlOutput = "";
+        $.each(data.message, function (dataKey, dataValue) {
+            htmlOutput += "<li>";
+            htmlOutput += "<img src='" + dataValue + "'/>";
+            htmlOutput += "</li>";
+        });
+
+        //Step 3e - send the content of HTML results variable to the HTML - display them in the html page (use "<pre><code>" to auto format the lyrics mode details here https: //www.w3schools.com/tags/tag_code.asp and here https://www.w3schools.com/tags/tag_pre.asp )
+        $('.js-search-results').html(htmlOutput);
+    }
+}
+
+
+//Step 1 - watch for user input; tell shopkeeper what shoe size, color
+function watchSubmit() {
+
+    //Step 1a - create a trigger
+    $(".js-search-form").submit(function (event) {
+
+        //if the page refreshes when you submit the form use "preventDefault()" to force JavaScript to handle the form submission
+        event.preventDefault();
+
+        //Step 1b - get user input - get the value from the input box
+        let queryTarget = $(event.currentTarget).find('.js-query').val();
+
+        //Step 1c - input validation - validate input
+        if (queryTarget == '') {
+            alert("Please select an artist");
+        }
+
+
+        //Step 1d - use the api function - use that artist and title values to call the getResults function defined at the top
+        getDataFromApi(queryTarget);
+    });
+}
+
+
+
+$(watchSubmit);
---
+++ readme.md
@@ -0,0 +1,55 @@
+# Song Lyrics API solution
+
+for the Dogs API white-board challenge (https://repl.it/@thinkful/dog-API-eval-question)
+
+### Live preview: https://mariusbanea.github.io/thinkful-song-lyrics-api
+
+# FAQs
+
+
+## How to build an API app?
+
+* Step 1 - watch for user input; tell shopkeeper what shoe size, color
+* - Step 1a - create a trigger
+* - Step 1b - get user input
+* - Step 1c - input validation
+* - Step 1d - use the api function
+
+
+* Step 2 - define the function to make the api call; shopkeeper goes to warehouse to get shoe
+* - Step 2a - make the api call using the URL, dataType (JSON or JSONP), type (GET or POST)
+* - Step 2b - success scenario (call the function to display the results)
+* - Step 2c - failure scenario (display errors)
+
+
+* Step 3 - display the results; sales process
+* - Step 3a - console.log the results
+* - Step 3b - if there are no results show errors
+* - Step 3c - if there are results, create an HTML results variable
+* - Step 3d - if there is more than one restul, use a for loop (or .each) to populate the empty HTML results variable
+* - Step 3e - send the content of HTML results variable to the HTML ($("class or id name from the HTML").html(HTML results variable))
+
+## What is AJAX?
+
+AJAX basics https://www.w3schools.com/xml/ajax_intro.asp
+
+## Where do I find the API documentation?
+
+https://dog.ceo/dog-api/#breed and select /api/breed/{breed name}/images
+
+
+## What is the API end point syntax?
+
+https://dog.ceo/api/breed/' + queryTarget + '/images,
+
+## Do I need an API key?
+
+No.
+
+## My page refreshes as soon as I click the button
+
+Use event.preventDefault(); (mode details here https://www.w3schools.com/jquery/event_preventdefault.asp)
+
+## How do I make the website accesible?
+
+Visit https://courses.thinkful.com/web-dev-002v1/project/1.2.2
